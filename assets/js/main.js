2c5e39dc55c1dd272e6f960f97207ec773119983
Michael Jones <mtj0273@gmail.com>
2022-08-03 18:34:01 -0400

    site now fully responsive! fixed navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,45 +1,47 @@
-// (function($) {
+(function($) {
 
-// 	var $window = $(window),
-// 		$body = $('body');
+	var $window = $(window),
+		$body = $('body');
 
-// 	// Breakpoints.
-// 		breakpoints({
-// 			xlarge:  [ '1281px',  '1680px' ],
-// 			large:   [ '981px',   '1280px' ],
-// 			medium:  [ '737px',   '980px'  ],
-// 			small:   [ null,      '736px'  ]
-// 		});
+	// Breakpoints.
+		breakpoints({
+			xlarge:  [ '1281px',  '1680px' ],
+			large:   [ '981px',   '1280px' ],
+			medium:  [ '737px',   '980px'  ],
+			small:   [ null,      '736px'  ]
+		});
 
-// 	// Nav.
+	// Nav.
 
-// 		// Title Bar.
-// 			$(
-// 				'<div id="titleBar">' +
-// 					'<a href="#navPanel" class="toggle"></a>' +
-// 					'<span class="title">' + $('#logo').html() + '</span>' +
-// 				'</div>'
-// 			)
-// 				.appendTo($body);
+		// Title Bar.
+			$(
+				'<div id="titleBar">' +
+                    "<a class='logo-container current-page-item nav-logo' href='index.html'>" +
+                        "<img id='logo' src='./logos/vector/default-monochrome.svg'>" +
+                    "</a>" +
+					'<a href="#navPanel" class="toggle"></a>' +
+				'</div>'
+			)
+				.appendTo($body);
 
-// 		// Navigation Panel.
-// 			$(
-// 				'<div id="navPanel">' +
-// 					'<nav>' +
-// 						$('#nav').navList() +
-// 					'</nav>' +
-// 				'</div>'
-// 			)
-// 				.appendTo($body)
-// 				.panel({
-// 					delay: 500,
-// 					hideOnClick: true,
-// 					hideOnSwipe: true,
-// 					resetScroll: true,
-// 					resetForms: true,
-// 					side: 'left',
-// 					target: $body,
-// 					visibleClass: 'navPanel-visible'
-// 				});
+		// Navigation Panel.
+			$(
+				'<div id="navPanel">' +
+					'<nav>' +
+						$('#nav').navList() +
+					'</nav>' +
+				'</div>'
+			)
+				.appendTo($body)
+				.panel({
+					delay: 500,
+					hideOnClick: true,
+					hideOnSwipe: true,
+					resetScroll: true,
+					resetForms: true,
+					side: 'left',
+					target: $body,
+					visibleClass: 'navPanel-visible'
+				});
 
-// })(jQuery);
+})(jQuery);
